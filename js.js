0d6f4927fb9b655e1e7ddecf3dec6f65ded5d42a
Dennis <dennisent@protonmail.com>
2017-12-10 08:32:55 +1300

    Add files via upload

--- js.js
+++ js.js
@@ -1,16 +1,20 @@
-$(document).ready(function() {
+$(document).ready(() => {
+  let input;
+  // Inputs array handles our incoming values and operators
   var inputs = [''];
   var totalString;
   var operators1 = ['+', '-', '*', '/'];
   var operators2 = ['.'];
   var nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+  // This program uses the eval() method which takes a string and evaluates it.
+
   function getValue(input) {
     if (
-      operators2.includes(inputs[inputs.length - 1] === true) &&
+      operators2.includes(inputs[inputs.length - 1]) === true &&
       input === '.'
     ) {
-      console.log("Duplicate '.' ");
+      $('error').html("Duplicate '.'");
     } else if (inputs.length === 1 && operators1.includes(inputs) === false) {
       inputs.push(input);
     } else if (operators1.includes(inputs[inputs.length - 1]) === false) {
@@ -23,19 +27,26 @@ $(document).ready(function() {
 
   function update() {
     totalString = inputs.join('');
-    $('#steps').html(totalString);
+    $('#calculation').html(totalString);
   }
 
+  // Runs the eval() method and displays result on screen:
+
   function getTotal() {
+    // Convert array to a string:
     totalString = inputs.join('');
-    $('#steps').html(eval(totalString));
+    $('#calculation').html(eval(totalString).toFixed(2));
   }
 
   $('a').on('click', function() {
-    if (this.id === 'deleteAll') {
+    if (this.id === 'AC') {
       inputs = [''];
-      update();
-    } else if (this.id === 'backOne') {
+      $('#calculation').html('0');
+    } else if (this.id === 'CE' && inputs.length <= 2) {
+      console.log(inputs.length);
+
+      $('#calculation').html('0');
+    } else if (this.id === 'CE') {
       inputs.pop();
       update();
     } else if (this.id === 'total') {
